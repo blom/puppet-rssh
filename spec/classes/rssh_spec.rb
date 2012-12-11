9b364d7bc70f1e052258c44bca7ce8d0d0a09805
Ørjan Blom <blom@blom.tv>
2012-12-11 18:19:42 +0100

    Add more tests

--- spec/classes/rssh_spec.rb
+++ spec/classes/rssh_spec.rb
@@ -1,22 +1,58 @@
 require "spec_helper"
 
 describe "rssh" do
+  let(:config_file) { "/etc/rssh.conf" }
+
   it { should contain_package("rssh").with_ensure("installed") }
-  it { should contain_file("/etc/rssh.conf").with_ensure("present").
-                                             with_mode("0644") }
+  it { should contain_file(config_file).
+              with_ensure("present").
+              with_mode("0644") }
 
-  context "when package is 'rssh_xyz'" do
-    let(:params) { {:package => "rssh_xyz"} }
-    it { should contain_package("rssh_xyz") }
-  end
+  context "parameters" do
+    context "when package is 'rssh_xyz'" do
+      let(:params) { {:package => "rssh_xyz"} }
+      it { should contain_package("rssh_xyz") }
+    end
 
-  context "when config_file is '/usr/local/etc/rssh.conf'" do
-    let(:params) { {:config_file => "/usr/local/etc/rssh.conf"} }
-    it { should contain_file("/usr/local/etc/rssh.conf") }
-  end
+    context "when config_file is '/usr/local/etc/rssh.conf'" do
+      let(:params) { {:config_file => "/usr/local/etc/rssh.conf"} }
+      it { should contain_file("/usr/local/etc/rssh.conf") }
+    end
 
-  context "when config_mode is '0600'" do
-    let(:params) { {:config_mode => "0600"} }
-    it { should contain_file("/etc/rssh.conf").with_mode("0600") }
+    context "when config_mode is '0600'" do
+      let(:params) { {:config_mode => "0600"} }
+      it { should contain_file(config_file).with_mode("0600") }
+    end
+
+    context "when allow contains 'one' and 'two'" do
+      let(:params) { {:allow => ["one", "two"]} }
+      it { should contain_file(config_file).
+                  with_content(/^allowone$/).
+                  with_content(/^allowtwo$/) }
+    end
+
+    context "when umask is '077'" do
+      let(:params) { {:umask => "077"} }
+      it { should contain_file(config_file).with_content(/^umask = 077$/) }
+    end
+
+    context "when logfacility is 'local0'" do
+      let(:params) { {:logfacility => "local0"} }
+      it { should contain_file(config_file).
+                  with_content(/^logfacility = local0$/) }
+    end
+
+    context "when chrootpath is '/chroot'" do
+      let(:params) { {:chrootpath => "/chroot"} }
+      it { should contain_file(config_file).
+                  with_content(%r{^chrootpath = /chroot$}) }
+    end
+
+    context "when users contains 'a' and 'b'" do
+      let(:params) { {:users => ["a", "b"]} }
+      it { should contain_file(config_file).
+                  with_content(/^user = "a"$/).
+                  with_content(/^user = "b"$/) }
+    end
   end
 end
